d2e6c3812930352328a251fb73588a32db6e65de
hectorhorna <u202113453@upc.edu.pe>
2023-11-04 14:03:02 -0500

    Implementacion del Botón Eliminar en ListarAdmin de ReservaEstacionamiento, e Implementación del CreaEdita para Registrar de ReservaEstacionamiento

--- SAFE_PARKING_FRONT/src/app/components/reserva-estacionamiento/creaedita-reserva-estacionamiento/creaedita-reserva-estacionamiento.component.ts
+++ SAFE_PARKING_FRONT/src/app/components/reserva-estacionamiento/creaedita-reserva-estacionamiento/creaedita-reserva-estacionamiento.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import * as moment from 'moment';
 import { HorarioEstacionamiento } from 'src/app/models/horarioEstacionamiento';
@@ -20,13 +20,18 @@ import { VehiculoService } from 'src/app/services/vehiculo.service';
 export class CreaeditaReservaEstacionamientoComponent {
   form: FormGroup = new FormGroup({});
   reservaestacionamiento: ReservaEstacionamiento = new ReservaEstacionamiento();
-  enable: boolean = true;
   mensaje: string = '';
   maxFecha: Date = moment().add(-1, 'days').toDate();
   listaUsuario: Usuario[] = [];
   listaVehiculo: Vehiculo[] = [];
   listaHorarioEst: HorarioEstacionamiento[] = [];
-  
+  id: number = 0;
+  edicion: boolean = false;
+
+  favoritos:{value:string;viewValue:string}[]=[
+    {value:'true',viewValue:'true'},
+    {value:'false',viewValue:'false'},
+  ]
   estado: { value: string; viewValue: string }[] = [
     { value: 'En proceso', viewValue: 'En Proceso' },
     { value: 'Completado', viewValue: 'Completado' },
@@ -34,12 +39,12 @@ export class CreaeditaReservaEstacionamientoComponent {
   constructor(
     private reS: ReservaEstacionamientoService,
     private uS: UsuarioService, // Asumiendo que tienes un servicio para Membresia
-    private vS:VehiculoService,
-    private heS:HorarioEstacionamientoService,
+    private vS: VehiculoService,
+    private heS: HorarioEstacionamientoService,
     private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -60,6 +65,34 @@ export class CreaeditaReservaEstacionamientoComponent {
       this.listaHorarioEst = data;
     });
   }
+
+
+  aceptar(): void {
+    if (this.form.valid) {
+      this.reservaestacionamiento.estado = this.form.value.estado;
+      this.reservaestacionamiento.favorito = this.form.value.favorito;
+      this.reservaestacionamiento.fecha = this.form.value.fecha;
+      this.reservaestacionamiento.users.idUsuario = this.form.value.users;
+      this.reservaestacionamiento.vehiculo.idVehiculo = this.form.value.vehiculo;
+      this.reservaestacionamiento.horarioEstacionamiento.idHorarioEstacionamiento = this.form.value.horarioEstacionamiento;
+      if (this.edicion) {
+        this.reS.update(this.reservaestacionamiento).subscribe(() => 
+        { this.reS.list().subscribe((data) => { 
+          this.reS.setList(data); 
+        }); 
+      });
+      }else{
+      this.reS.insert(this.reservaestacionamiento).subscribe((data) => {
+        this.reS.list().subscribe((data) => {
+          this.reS.setList(data);
+        });
+      });
+}
+      this.router.navigate(['/reservaestacionamiento/listar_admin_reserva_estacionamientos']);
+    } else {
+      this.mensaje = 'Por favor complete todos los campos obligatorios.';
+    }
+  }  
   obtenerControlCampo(nombreCampo: string): AbstractControl {
     const control = this.form.get(nombreCampo);
     if (!control) {
@@ -68,25 +101,19 @@ export class CreaeditaReservaEstacionamientoComponent {
     return control;
   }
 
-  aceptar(): void {
-    if (this.form.valid) {
-      this.reservaestacionamiento.estado = this.form.value.estado;
-      this.reservaestacionamiento.favorito = this.form.value.enable;
-      this.reservaestacionamiento.fecha = this.form.value.fecha;
-      this.reservaestacionamiento.users.idUsuario = this.form.value.users;
-      this.reservaestacionamiento.vehiculo.idVehiculo = this.form.value.vehiculo;
-      this.reservaestacionamiento.horarioEstacionamiento.idHorarioEstacionamiento = this.form.value.horarioEstacionamiento;
-
-
-      this.reS.insert(this.reservaestacionamiento).subscribe((data) => {
-        this.reS.list().subscribe((data) => {
-          this.reS.setList(data);
+  init(){
+    if(this.edicion){
+      this.reS.listId(this.id).subscribe((data)=>{
+        this.form=new FormGroup({
+          idReservaEstacionamiento:new FormControl(data.idReservaEstacionamiento),
+          estado:new FormControl(data.estado),
+          favorito:new FormControl(data.favorito),
+          fecha:new FormControl(data.fecha),
+          users:new FormControl(data.users),
+          vehiculo:new FormControl(data.vehiculo),
+          horarioEstacionamiento:new FormControl(data.horarioEstacionamiento),
         });
       });
-
-      this.router.navigate(['reservaestacionamiento/listar_admin_reserva_estacionamientos']);
-    } else {
-      this.mensaje = 'Por favor complete todos los campos obligatorios.';
     }
   }
 }
