1ee685c4c3c761f78cec4d966788873320f80a91
DSR124 <246810diegosr@gmail.com>
2023-11-08 13:49:45 -0500

    mejora en el diseño de cada entidad para el navegador

--- SAFE_PARKING_FRONT/src/app/components/navbar-administrador/navbar-administrador.component.ts
+++ SAFE_PARKING_FRONT/src/app/components/navbar-administrador/navbar-administrador.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterEvent } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-navbar-administrador',
   templateUrl: './navbar-administrador.component.html',
-  styleUrls: ['./navbar-administrador.component.css']
+  styleUrls: ['./navbar-administrador.component.css'],
 })
 export class NavbarAdministradorComponent {
-  constructor(public route: ActivatedRoute, private router: Router) {}
+  role: string = '';
+  constructor(
+    public route: ActivatedRoute,
+    private router: Router,
+    private loginService: LoginService
+  ) {}
+
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+  validarRol() {
+    if (this.role == 'administrador') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
