ee28a78249025aa1df0de0f7f765c664af84489c
Jhon Harold Sikos Astete <haroldsikos@gmail.com>
2023-11-17 18:24:04 -0500

    Despliegue Backend cambios

--- SAFE_PARKING_FRONT/src/environments/environment.ts
+++ SAFE_PARKING_FRONT/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  base_datos: 'https://safe-parking-deployment-2023.onrender.com:5432',
+  base_datos: 'https://safe-parking-deployment-2023.onrender.com',
   //http://localhost:8080
 };
