1f514285957b59889f86b56814af125acbf2daaa
Jhon Harold Sikos Astete <haroldsikos@gmail.com>
2023-11-17 14:59:24 -0500

    package json

--- SAFE_PARKING_FRONT/src/environments/environment.ts
+++ SAFE_PARKING_FRONT/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  base_datos: 'https://safe-parking-deployment-2023.onrender.com',
+  base_datos: 'https://safe-parking-deployment-2023.onrender.com:5432',
   //http://localhost:8080
 };
