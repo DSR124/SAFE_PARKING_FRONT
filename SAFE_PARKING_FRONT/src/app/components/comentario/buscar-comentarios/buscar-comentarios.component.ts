dbaf246eb1ac94f2a004973887922b578caf2ccb
EliasJMZegarra <105391465+EliasJMZegarra@users.noreply.github.com>
2023-11-18 16:46:20 -0500

    Actualización del responsive en  comentarios/buscar-comentarios
    Actualización del responsive en  comentarios/buscar-comentarios

--- SAFE_PARKING_FRONT/src/app/components/comentario/buscar-comentarios/buscar-comentarios.component.ts
+++ SAFE_PARKING_FRONT/src/app/components/comentario/buscar-comentarios/buscar-comentarios.component.ts
@@ -53,4 +53,12 @@ export class BuscarComentariosComponent implements OnInit {
       return false;
     }
   }
+  //Para ocultar la barra
+
+  mostrarNavbar = false; // Variable de estado para controlar la visibilidad de la barra
+
+  toggleNavbar() {
+    this.mostrarNavbar = !this.mostrarNavbar;
+  }
+  //Fin de ocultar la barra
 }
